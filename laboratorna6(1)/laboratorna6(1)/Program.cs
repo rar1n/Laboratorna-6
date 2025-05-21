2a112c764c2681c222f2beacd574ba07e12ff949
rar1n <bolbat.ilya13@gmail.com>
2025-05-21 17:18:33 +0300

    Л.Р.6 Завдання 1 (2 варіант)
    Л.Р.6 Завдання 1 (2 варіант)

---
+++ laboratorna6(1)/laboratorna6(1)/Program.cs
@@ -0,0 +1,22 @@
+﻿Random o = new Random();
+List<int> chisla1 = new List<int>();
+Console.Write("Введiть N (кiлькiсть чисел): ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сформована колекцiя: ");
+for (int i = 0; i < N; i++)
+{
+    chisla1.Add(o.Next(-5, 6));
+    Console.Write("{0} ", chisla1[i]);
+}
+Console.WriteLine("\nКолекцiя пiсля видалення негативних чисел: ");
+for (int i = chisla1.Count - 1; i >= 0; i--)
+{
+    if (chisla1[i] < 0)
+    {
+        chisla1.RemoveAt(i);
+    }
+}
+foreach (int chislo in chisla1)
+{
+    Console.Write("{0} ", chislo);
+}
